b3edab686230792644047d9f5f6b54b36a18436c
Danilo Cividin <danilo.cividin.ext@infocert.it>
2024-01-29 16:46:16 +0100

    Assignment: create Get Customer

---
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import guru.springframework.msscbrewery.services.CustomerService;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/customer")
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/{customerId}")
+    public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID customerId) {
+        return ResponseEntity.ok(customerService.getCustomerById(customerId));
+    }
+}
