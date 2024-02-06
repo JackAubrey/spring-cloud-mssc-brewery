747cecd40837a213688f956795ec9be7b85e3367
danilo <d.cividin@hotmail.it>
2024-02-06 11:25:02 +0100

    added DateConverter used by BeerMapper

--- src/main/java/guru/springframework/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/msscbrewery/web/mappers/BeerMapper.java
@@ -1,10 +1,11 @@
 package guru.springframework.msscbrewery.web.mappers;
 
 import guru.springframework.msscbrewery.model.Beer;
+import guru.springframework.msscbrewery.web.converters.DateConverter;
 import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateConverter.class})
 public interface BeerMapper {
     BeerDtoV2 beerToBeerDtoV2(Beer beer);
     Beer beerDtoV2ToBeer(BeerDtoV2 beerDtoV2);
--- src/main/java/guru/springframework/msscbrewery/web/model/v2/BeerDtoV2.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/v2/BeerDtoV2.java
@@ -1,10 +1,15 @@
 package guru.springframework.msscbrewery.web.model.v2;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Data
@@ -12,8 +17,15 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDtoV2 {
+    @Null
     private UUID id;
+    @NotBlank(message = "The Beer Name is mandatory")
     private String beerName;
+    @NotNull(message = "The Beer Style is mandatory")
     private BeerStyleEnum beerStyle;
+    @Positive
     private Long upc;
+
+    private Timestamp createdDate;
+    private Timestamp lastUpdatedDate;
 }
